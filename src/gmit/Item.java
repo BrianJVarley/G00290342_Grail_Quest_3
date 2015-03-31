186e27955e0444b7f4cc25f13c33c5b65d0ee4fa
BrianJVarley <brianvarley9@gmail.com>
2015-03-31 17:19:54 +0100

    March 31 - Added extra POJO's

---
+++ src/gmit/Item.java
@@ -0,0 +1,50 @@
+package gmit;
+
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Item {
+	
+	private Item[] item;
+	
+	@JsonProperty("@name")
+    private String name;
+	
+	@JsonProperty("@locationID")
+    private String locationID;
+
+	public Item[] getItem() {
+		return item;
+	}
+
+	public void setItem(Item[] item) {
+		this.item = item;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocationID() {
+		return locationID;
+	}
+
+	public void setLocationID(String locationID) {
+		this.locationID = locationID;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [item=" + Arrays.toString(item) + ", name=" + name
+				+ ", locationID=" + locationID + "]";
+	}
+	
+	
+	
+
+}
