1325d2c4c1027cc88f0c73e819136ea09659487c
BrianJVarley <brianvarley9@gmail.com>
2015-04-10 15:21:58 +0100

    April 10 - Added grpah class

---
+++ src/gmit/Graph.java
@@ -0,0 +1,44 @@
+package gmit;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Graph {
+
+    private class Vertex {
+        Map<String,Object> properties;
+        private Map<String,Edge> edges;
+
+        public Graph addVertex(String edgeName, Vertex v) {
+            Edge e = edges.get(edgeName);
+            if(e == null) {
+                e = new Edge(this);
+                edges.put(edgeName,e);
+            }
+
+            e.addVertex(v);
+            return Graph.this;
+        }
+
+        public Graph addVertex(Vertex v) {
+            return addVertex("anonymous",v);
+        }
+
+    }
+
+    private static class Edge {
+        Map<String,Object> properties;
+        Vertex in;
+        Collection<Vertex> out;
+
+        private Edge(Vertex in) {
+            this.in = in;
+        }
+
+
+        private void addVertex(Vertex v) {
+            out.add(v);
+        }
+    }
+
+}
